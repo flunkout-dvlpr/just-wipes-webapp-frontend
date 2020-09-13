8b2d4bde60dd4a6bb734833d4b27499cbb890a87
flunkout-dvlpr <info@juliogonzalez.space>
2020-09-13 18:29:17 -0500

    Add routes for Profile/Home

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,10 +4,18 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Home.vue') }
+      {
+        name: 'home',
+        path: '',
+        component: () => import('pages/Home.vue')
+      },
+      {
+        name: 'profile',
+        path: '/profile',
+        component: () => import('pages/Profile.vue')
+      }
     ]
   },
-
   // Always leave this as last one,
   // but you can also remove it
   {
