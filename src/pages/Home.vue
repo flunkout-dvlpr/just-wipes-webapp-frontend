a799c282769ca50035240bcd0820fc4a7dd661f7
flunkout-dvlpr <info@juliogonzalez.space>
2020-09-13 18:29:54 -0500

    Import signin componenet to Home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,21 @@
 <template>
   <q-page class="flex flex-center">
+    <SignIn></SignIn>
   </q-page>
 </template>
 
 <script>
+import SignIn from 'components/SignIn'
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  components: {
+    SignIn
+  }
 }
 </script>
+
+<style>
+/*.q-page {
+  min-height: 656px !important;
+}*/
+</style>
