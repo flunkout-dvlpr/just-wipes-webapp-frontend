fe4241afd0fa114072db2132c8c252a133c60651
flunkout-dvlpr <info@juliogonzalez.space>
2020-09-13 18:28:33 -0500

    Add logo and shopping cart button, logo routes to home on click

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,10 +1,24 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="lHh Lpr lFf" class="bg-primary">
     <q-header elevated>
-      <q-toolbar>
-        <q-toolbar-title>
-          Just Wipes
+      <q-toolbar class="row bg-grey-1">
+        <q-toolbar-title class="text-grey-9">
+          <div @click="goHome">
+            <img
+              src="~assets/justWipes.png"
+              class="q-pt-xs"
+            />
+          </div>
         </q-toolbar-title>
+        <q-btn
+          size="lg"
+          flat round dense
+          color="grey-9"
+          icon="shopping_cart"
+          type="a"
+          target="_blank"
+          href="https://justwipes.co/"
+        />
       </q-toolbar>
     </q-header>
     <q-page-container>
@@ -15,6 +29,11 @@
 
 <script>
 export default {
-  name: 'MainLayout'
+  name: 'MainLayout',
+  methods: {
+    goHome () {
+      this.$router.push({ name: 'home' })
+    }
+  }
 }
 </script>
