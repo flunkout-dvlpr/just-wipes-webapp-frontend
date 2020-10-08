e0b97bc097db649daed1d371d856c5ae8d796dcd
flunkout-dvlpr <info@juliogonzalez.space>
2020-10-08 17:44:16 -0500

    Add Notify and Dialog plugins, add amplify and axios boot files

--- quasar.conf.js
+++ quasar.conf.js
@@ -19,8 +19,8 @@ module.exports = function (/* ctx */) {
     // --> boot files are part of "main.js"
     // https://quasar.dev/quasar-cli/boot-files
     boot: [
-
-      'axios'
+      'axios',
+      'amplify'
     ],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
@@ -100,7 +100,8 @@ module.exports = function (/* ctx */) {
 
       // Quasar plugins
       plugins: [
-        'Notify'
+        'Notify',
+        'Dialog'
       ]
     },
 
