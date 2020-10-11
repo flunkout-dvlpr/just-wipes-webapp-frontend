137f6c0ad5999fcdad61da351db921996495299f
flunkout-dvlpr <info@juliogonzalez.space>
2020-10-11 17:21:15 -0500

    Create component for Info tab

---
+++ src/components/Info.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="text-h6">Info</div>
+    Educate the user on the ingredients used in wipes and tips on properly disinfecting
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
