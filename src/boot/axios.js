a04f3277c4dad1c7cdb5c35e81bef65b49b3a7b7
flunkout-dvlpr <info@juliogonzalez.space>
2020-10-11 17:32:41 -0500

    Set default api baseUrl for prod/dev, set global token to ref vuex user module

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,13 @@
 import Vue from 'vue'
 import axios from 'axios'
 
+if (process.env.PROD) {
+  axios.defaults.baseURL = 'https://API.execute-api.us-east-2.amazonaws.com/Prod/'
+} else {
+  axios.defaults.baseURL = 'http://localhost:3000/'
+}
+export default function ({ store }) {
+  console.log(store.state.user.token)
+  axios.defaults.headers.common.Authorization = `Bearer ${store.state.user.token}`
+}
 Vue.prototype.$axios = axios
