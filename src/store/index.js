9d7ee5efb10603fab654d5c3ae32c7b66e003901
flunkout-dvlpr <info@juliogonzalez.space>
2020-10-13 10:22:08 -0500

    Import  persistedstate plugin

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import user from './user'
+import createPersistedState from 'vuex-persistedstate'
 // import example from './module-example'
 
 Vue.use(Vuex)
@@ -19,7 +20,9 @@ export default function (/* { ssrContext } */) {
     modules: {
       user
     },
-
+    plugins: [createPersistedState({
+      storage: window.sessionStorage
+    })],
     // enable strict mode (adds overhead!)
     // for dev mode only
     strict: process.env.DEV
