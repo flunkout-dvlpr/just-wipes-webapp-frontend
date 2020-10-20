5e47f423da48b4233e6f8d5514f3431f688b3875
flunkout-dvlpr <info@juliogonzalez.space>
2020-10-20 16:47:05 -0500

    Create mutation to reset user vuex state

--- src/store/user/mutations.js
+++ src/store/user/mutations.js
@@ -10,3 +10,9 @@ export function setToken (state, payload) {
 export function setData (state, payload) {
   state.data = payload
 }
+
+export function resetState (state) {
+  state.user = null
+  state.token = null
+  state.data = null
+}
