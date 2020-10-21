f6934a634a8fef2384d3935c453d355e7b916105
flunkout-dvlpr <info@juliogonzalez.space>
2020-10-20 19:09:56 -0500

    Create shop tab and mock up Just Wipes Products with q-cards and images

---
+++ src/components/Shop.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="row flex-center q-col-gutter-lg items-stretch">
+<!--     <div class="q-gutter-xl">
+      <q-card style="height: auto; max-width: 40vh">
+        <q-card-section>
+          Some Text
+        </q-card-section>
+      </q-card>
+    </div> -->
+    <div v-for="product in products" :key="product.id">
+      <q-card style="height: 42vh; width: 40vh">
+        <q-img
+          :src="product.image"
+          style="height: 30vh; width: 40vh"
+        />
+
+        <q-card-actions>
+          <div class="col-10" align="left">
+            <div class="text-h6">{{ product.label }}</div>
+            <div class="text-subtitle">{{ product.subtitle }}</div>
+          </div>
+          <div class="col-2">
+            <q-btn
+              flat
+              class="col-12"
+              :color="product.color"
+              icon="shopping_basket"
+              size="lg"
+            />
+            <div class="text-center col-12">{{ product.price }}</div>
+          </div>
+        </q-card-actions>
+      </q-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Shop',
+  data () {
+    return {
+      products: [
+        {
+          label: 'Just Wipes',
+          subtitle: '(70% Isopropyl, 30% Water and Aloe Extract)',
+          image: '/just.png',
+          color: 'brand-teal',
+          price: '$0.00',
+          link: ''
+        },
+        {
+          label: 'Plane Wipes - 6 Pack',
+          subtitle: '(70% Isopropyl Alcohol)',
+          image: '/plane.png',
+          color: 'brand-orange',
+          price: '$0.00',
+          link: ''
+        },
+        {
+          label: 'Mat Wipes',
+          subtitle: '',
+          image: '/mat.png',
+          color: 'brand-yellow',
+          price: '$0.00',
+          link: ''
+        },
+        {
+          label: 'Industry Wipes',
+          subtitle: '',
+          image: '/industry.png',
+          color: 'brand-orange',
+          price: '$0.00',
+          link: ''
+        },
+        {
+          label: 'Sex Wipes',
+          subtitle: '',
+          image: '/sex.png',
+          color: 'brand-purple',
+          price: '$0.00',
+          link: ''
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
